f5a825d3e0137060e8aad0cba4caf51974ad8581
naveen-creater <Naveenkumar-creater>
2025-04-11 09:18:02 +0530

    removed unused package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
